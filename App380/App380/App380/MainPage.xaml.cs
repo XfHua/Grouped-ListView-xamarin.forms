b6c28bb7a3e2d394ab8ad318b97c25e02f07ae72
XfHua <jasonHuaxf@163.com>
2019-05-30 09:31:27 +0800

    123

---
+++ App380/App380/App380/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App380
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+
+            listView.ItemsSource = new List<ContactGroup> {
+                new ContactGroup("P", "P")
+                {
+                   new Contact { Name = "Peter Parker", Status = "Nice to meet you!" }
+                },
+                new ContactGroup("J", "J")
+                {
+                   new Contact { Name = "John Smith", Status = "Hey, let's talk!" }
+                }
+            };
+        }
+    }
+    public class Contact
+    {
+        public string Name { get; set; }
+        public string Status { get; set; }
+
+    }
+
+    public class ContactGroup : List<Contact>
+    {
+        public string Title { get; set; }
+        public string ShortTitle { get; set; }
+
+
+        public ContactGroup(string title, string shortTitle)
+        {
+            Title = title;
+            ShortTitle = shortTitle;
+        }
+
+    }
+}
